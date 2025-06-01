9a1da7394e8ad446c80383bb7ec5223ea2ecea55
Nitish Kumar <nik.nilay27@gmail.com>
2025-06-02 04:15:48 +0530

    committing the completed excercises

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str.split('').reverse().join('');
 };
+
+const result = reverseString('hello');
+const result1 = reverseString('hello there');
+const result2 = reverseString('123! abc! Hello, Odinite.');
+const result3 = reverseString('');
+// console.log(result);
+
+// const re
 
 // Do not edit below this line
 module.exports = reverseString;
