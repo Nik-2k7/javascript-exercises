080259f48cd0a7d375988b9e78d0e0d9fa636e88
Nitish Kumar <nik.nilay27@gmail.com>
2025-07-25 15:32:06 +0530

    completed palindrome excercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleaned = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reversed = cleaned.split('').reverse().join('');
+    return cleaned === reversed;
 };
 
 // Do not edit below this line
